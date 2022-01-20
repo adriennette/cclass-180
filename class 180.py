6d5d6c72534d5ef9334be3e0238ba09e48d9706a
adriennette <92022862+adriennette@users.noreply.github.com>
2022-01-20 19:55:57 +0530

    Add files via upload

---
+++ class 180.py
@@ -0,0 +1,23 @@
+from tkinter import *
+import speech_recognition as sr
+root = Tk()
+root.geometry("500x500")
+def r_audio():
+    speech_recognisor= sr.Recognizer()
+    with sr.Microphone() as source:
+        audio=  speech_recognisor.listen(source)
+        voice_data=''
+        try:
+            voice_data=  speech_recognisor.recognize_google(audio, language='en-in')
+        except sr.UnknownValueError:
+            print('Please repeat i did not get that')
+    print(voice_data)
+   
+r_audio()
+root.mainloop()
+"""
+Created on Thu Jan 20 19:35:43 2022
+
+@author: Admin
+"""
+
